f02b794af87a3fcc2ba7aab11b24ab0380036cf6
Abreham <abrehamessa@yahoo.com>
2014-06-30 23:13:48 -0700

    clean some dead wood

--- src/com/thought/exercise/client/SchedulerProgram.java
+++
@@ -1,6 +0,0 @@
-package com.thought.exercise.client;
-
-public class SchedulerProgram {
-	public static void main(String[] args) {
-	}
-}
--- src/com/thought/exercise/utils/IValidator.java
+++
@@ -1,6 +0,0 @@
-package com.thought.exercise.utils;
-
-public interface IValidator {
-	
-	boolean isValid(String input);
-}
